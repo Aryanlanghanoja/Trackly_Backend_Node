e18e0a1b78609e01e5787cc52fc0bbb9bf379e34
Aryanlanghanoja <aryanlanghanoja233@gmail.com>
2025-04-26 23:38:16 +0530

    added model for document

---
+++ model/document.model.js
@@ -0,0 +1,62 @@
+const { DataTypes } = require('sequelize');
+
+// Exporting the model definition function
+module.exports = model;
+
+/**
+ * Defines the 'Documents' model with the required attributes and their properties.
+ * 
+ * @param {Sequelize} sequelize - The Sequelize instance for database connection
+ * @returns {Model} The Sequelize model for 'Documents'
+ */
+function model(sequelize) {
+    const attributes = {
+        // Primary key for the document
+        documet_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        // Foreign key from Leads table
+        lead_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'leads',  // Ensure there's a 'Leads' table/model defined
+                key: 'lead_id'
+            }
+        },
+
+        // Foreign key from Leads table
+        task_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'task',  // Ensure there's a 'Leads' table/model defined
+                key: 'task_id'
+            }
+        },
+
+        // Path to the document file
+        doc_path: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        // Name of the document
+        doc_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        // Description of the document
+        doc_desc: {
+            type: DataTypes.TEXT,
+            allowNull: true
+        }
+    };
+
+    return sequelize.define("document", attributes);
+}
