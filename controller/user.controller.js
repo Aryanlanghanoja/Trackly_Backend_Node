0022571fae4e7bcbec075781bb3a0d0e68c8d1ee
ritzsanchala2607 <ritzsanchala2607@gmail.com>
2025-05-15 20:54:11 +0530

    Solved Merge Conflict

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -223,20 +223,13 @@ const deleteProfile = async(req, res) => {
         const picture = decoded.profile_photo;
 
         if (picture) {
-<<<<<<< Updated upstream
-            const oldPath = path.join(__dirname, "..",  "public", picture);
-=======
-<<<<<<< HEAD
             const oldPath = path.join(__dirname, "..", "public", picture);
-            console.log(oldPath);
-=======
-            const oldPath = path.join(__dirname, "..",  "public", picture);
->>>>>>> 8cf4d274cb239384c860a51e796776f08991bdb4
->>>>>>> Stashed changes
+            console.log(oldPath); // keep or remove based on need
             if (fs.existsSync(oldPath)) {
                 fs.unlinkSync(oldPath);
             }
         }
+
 
         await userService.deleteUserById(decoded.id);
 
