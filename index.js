28f41d8cafe563785436f96df90384db9f4568fe
Aryanlanghanoja <aryanlanghanoja233@gmail.com>
2025-05-16 07:02:49 +0530

    working on login

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const serverless = require("serverless-http");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const session = require("express-session");
+// const session = require("express-session");
 const path = require("path");
 
 const passport = require("passport");
@@ -44,18 +44,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      secure: false,
-      httpOnly: true,
-      sameSite: "lax",
-    },
-  })
-);
+// app.use(
+//   session({
+//     secret: process.env.SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {
+//       secure: false,
+//       httpOnly: true,
+//       sameSite: "lax",
+//     },
+//   })
+// );
 
 app.use(passport.initialize());
 app.use(passport.session());
